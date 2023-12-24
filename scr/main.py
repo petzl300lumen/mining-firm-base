2b9c6df6418cc51d485be556c49c0a5ba6ae58e5
petzl300lumen <Ftgaq007@gmail.com>
2023-12-25 01:56:26 +0300

    Update main.py

--- scr/main.py
+++ scr/main.py
@@ -6,8 +6,22 @@ app = FastAPI()
 
 @app.get('/')
 def root():
-    #return {"message": "Hello world"}
+    return {"message": "Hello world"}
     return RedirectResponse('/docs')
+
+@app.get('/users')
+def get():
+    return {"message": "def users"}
+
+@app.get('/users/{user_id}/')
+def get_user_id(user_id:int):
+    return {
+        "user":{
+            "id": user_id,
+        },
+    }
 
 if __name__ == '__main__':
     uvicorn.run(app='main:app', host="0.0.0.0", port=8000, reload=True)
+    
+    #http://127.0.0.1:8000/

b6cc37f94523b1b1026c447eee36aeacccec061b
petzl300lumen <Ftgaq007@gmail.com>
2023-12-25 20:48:42 +0300

    Update main.py

--- scr/main.py
+++ scr/main.py
@@ -1,25 +1,17 @@
 from fastapi import FastAPI
 import uvicorn
 from fastapi.responses import RedirectResponse
+from users.routers import router as users_router
 
 app = FastAPI()
+
+app.include_router(users_router, prefix='/users') #main prefix
 
 @app.get('/')
 def root():
     return {"message": "Hello world"}
     return RedirectResponse('/docs')
 
-@app.get('/users')
-def get():
-    return {"message": "def users"}
-
-@app.get('/users/{user_id}/')
-def get_user_id(user_id:int):
-    return {
-        "user":{
-            "id": user_id,
-        },
-    }
 
 if __name__ == '__main__':
     uvicorn.run(app='main:app', host="0.0.0.0", port=8000, reload=True)

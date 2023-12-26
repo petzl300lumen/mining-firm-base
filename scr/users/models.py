7f0cecd0c6f17a188dfde2f94b6eaca44ef0c54f
petzl300lumen <Ftgaq007@gmail.com>
2023-12-26 03:43:35 +0300

    Update models.py

--- scr/users/models.py
+++ scr/users/models.py
@@ -3,12 +3,16 @@ from typing import Optional
 
 class UserClass(BaseModel):
     id_user:int
-    login:Optional[int]
+    login:Optional[str]
 
 class UserPost(BaseModel):
     id_user:int
-    login:Optional[int]
+    login:Optional[str]
     password:Optional[int]
     phone_number: int
     email_address: str
     verified: int
+    
+class UserPut(BaseModel):
+    id_user: int
+    phone_number: int
